725938311c35da6d41d74d08521c71261d856eac
NishithaNair <nishitha@nalashaa.com>
2013-02-06 18:17:01 +0530

    Initial Commit

---
+++ projects/brandnew/Components/ClassLibrary2/ClassLibrary2/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public string Rename(string name)
+        {
+            return "Mrs" + name;
+        }
+    }
+}
